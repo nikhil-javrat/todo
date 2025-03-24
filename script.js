c732dca4ae03de79827e049f355bdbca9b4d7a52
nikhil-javrat <niyash034@gmail.com>
2025-03-24 19:05:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask() {
+  if (inputbox.value === '') {
+    alert(" you must write something!");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputbox.value = "";
+  savedata();
+}
+
+listcontainer.addEventListener("click", function(event){
+  if (event.target.tagName === "LI"){
+    event.target.classList.toggle("checked");
+    savedata();
+  }
+  else if (event.target.tagName === "SPAN"){
+    event.target.parentElement.remove();
+    savedata();
+  }
+}, false);
+ 
+function savedata(){
+  localStorage.setItem("data",listcontainer.innerHTML );
+}
+function showtask(){
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
